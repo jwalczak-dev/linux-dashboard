a1f60952928654420fc634f0f10de1e7443ccdfe
jakub walczak <jakubwalczak@op.pl>
2024-03-04 19:09:46 +0100

    Removing unnecessary code; Adding navigation bar component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavigationBarComponent} from "./navigation-bar/navigation-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Linux Dashboard';
 }
---
+++ frontend/src/app/navigation-bar/navigation-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navigation-bar.component.html',
+  styleUrl: './navigation-bar.component.scss'
+})
+export class NavigationBarComponent {
+
+}
