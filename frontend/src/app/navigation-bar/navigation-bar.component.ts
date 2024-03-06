077c3d9a1f14a6c95c2259d15000a8901ae2d67c
jakub walczak <jakubwalczak@op.pl>
2024-03-06 18:15:31 +0100

    Preparing a very basic navigation bar structure

--- frontend/src/app/navigation-bar/navigation-bar.component.ts
+++ frontend/src/app/navigation-bar/navigation-bar.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgFor} from '@angular/common';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [],
-  templateUrl: './navigation-bar.component.html',
+  imports: [NgFor],
+  template: `
+    <div class="navigation-bar-container">
+      <ul>
+        <li *ngFor="let tab of tabs">{{tab}}</li>
+      </ul>
+    </div>
+  `,
   styleUrl: './navigation-bar.component.scss'
 })
 export class NavigationBarComponent {
+  tabs: string[] = ['System', 'Network', 'Logs'];
 
 }
