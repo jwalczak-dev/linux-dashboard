0931aaf2cc2961ad0f968f4f65d737d91fa7d7a3
jakub walczak <jakubwalczak@op.pl>
2024-03-08 07:59:14 +0100

    Adding footer component to the frontend project

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavigationBarComponent} from "./navigation-bar/navigation-bar.component";
 import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationBarComponent, HeaderComponent],
+  imports: [RouterOutlet, NavigationBarComponent, HeaderComponent, FooterComponent],
   template: `
     <app-header [title]="title"></app-header>
     <app-navigation-bar></app-navigation-bar>
     <router-outlet/>
+    <app-footer></app-footer>
   `,
   styleUrl: './app.component.scss'
 })
