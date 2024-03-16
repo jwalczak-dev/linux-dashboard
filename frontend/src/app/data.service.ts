07d12849d3fae3c787f64e3a8c9f08c646708879
jakub walczak <jakubwalczak@op.pl>
2024-03-16 13:29:08 +0100

    Adding simple stream for cpu usage information

--- frontend/src/app/data.service.ts
+++ frontend/src/app/data.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {Observable, Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  #cpuUsageData$ = new Subject<any>();
+
   constructor() { }
+
+  getCpuUsageData(): Observable<any> {
+    return this.#cpuUsageData$.asObservable()
+  }
+
+  updateCpuUsageData(data: any): void {
+    this.#cpuUsageData$.next(data)
+  }
+
+
 }
