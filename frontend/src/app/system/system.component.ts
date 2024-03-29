124c4e3991cef34357d6fbcdb1dbc2d8cdd90e31
jakub walczak <jakubwalczak@op.pl>
2024-03-29 18:21:57 +0100

    Adding component for system informations

---
+++ frontend/src/app/system/system.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-system',
+  standalone: true,
+  imports: [],
+  templateUrl: './system.component.html',
+  styleUrl: './system.component.scss'
+})
+export class SystemComponent {
+
+}
