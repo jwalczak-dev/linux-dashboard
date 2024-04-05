bfbb61ab3e83439cb276392cff3cb17b2f936248
jakub walczak <jakubwalczak@op.pl>
2024-04-05 15:28:40 +0200

    Moving the "Logs" component template to a typescript file

--- frontend/src/app/logs/logs.component.ts
+++ frontend/src/app/logs/logs.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-logs',
   standalone: true,
   imports: [],
-  templateUrl: './logs.component.html',
+  template: `
+    <p>logs works!</p>
+  `,
   styleUrl: './logs.component.scss'
 })
 export class LogsComponent {
