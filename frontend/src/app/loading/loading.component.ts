5b1894ae7c864c4f0cdf907f07afaddb0f361fd9
jakub walczak <jakubwalczak@op.pl>
2024-04-11 10:15:59 +0200

    Moving the "Loading" component template to a typescript file

--- frontend/src/app/loading/loading.component.ts
+++ frontend/src/app/loading/loading.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-loading',
   standalone: true,
   imports: [],
-  templateUrl: './loading.component.html',
+  template: `
+    <p>loading works!</p>
+  `,
   styleUrl: './loading.component.scss'
 })
 export class LoadingComponent {
