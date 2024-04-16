900f6918be30388e00251ed527f6c3bb876ce6e0
jakub walczak <jakubwalczak@op.pl>
2024-04-16 19:22:35 +0200

    Removing unused code

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavigationBarComponent} from "./navigation-bar/navigation-bar.component";
 import {HeaderComponent} from "./header/header.component";
@@ -45,11 +45,7 @@ import {LoadingComponent} from "./loading/loading.component";
   `,
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Linux Dashboard';
   showLoader: boolean = true;
-
-  ngOnInit(): void {
-
-  }
 }
