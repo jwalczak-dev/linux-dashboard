bcee66207cd03cec68851ee11188c16a46e8e48e
jakub walczak <jakubwalczak@op.pl>
2024-05-18 05:56:52 +0200

    Updating network header

--- frontend/src/app/network/network.component.ts
+++ frontend/src/app/network/network.component.ts
@@ -1,15 +1,15 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 
 @Component({
   selector: 'app-network',
   standalone: true,
   imports: [],
   template: `
-    <p>network works!</p>
+    <p>{{componentHeader()}}</p>
   `,
   styleUrl: './network.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NetworkComponent {
-
+  componentHeader = signal('Network information')
 }
