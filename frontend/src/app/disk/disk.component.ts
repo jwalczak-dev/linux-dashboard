05b5e02d60dc9d97b52b942e1361b90e1e99ad63
jakub walczak <jakubwalczak@op.pl>
2024-05-19 06:31:38 +0200

    Updating the Disk component header

--- frontend/src/app/disk/disk.component.ts
+++ frontend/src/app/disk/disk.component.ts
@@ -1,15 +1,16 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 
 @Component({
   selector: 'app-disk',
   standalone: true,
   imports: [],
   template: `
-    <p>disk works!</p>
+    <p>{{componentHeader()}}</p>
   `,
   styleUrl: './disk.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DiskComponent {
+  componentHeader = signal('Disk information')
 
 }
