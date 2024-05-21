5f550cb19b2b5ec8cfce567d6cf281dc56f3ed2c
jakub walczak <jakubwalczak@op.pl>
2024-05-21 17:01:26 +0200

    Updating the System component header

--- frontend/src/app/system/system.component.ts
+++ frontend/src/app/system/system.component.ts
@@ -1,15 +1,16 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 
 @Component({
   selector: 'app-system',
   standalone: true,
   imports: [],
   template: `
-    <p>system works!</p>
+    <p>{{componentHeader()}}</p>
   `,
   styleUrl: './system.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SystemComponent {
+  componentHeader = signal('System information')
 
 }
